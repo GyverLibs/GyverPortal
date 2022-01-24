8b463ae62694dc1c4e052a12fc843e0429bd55f5
GyverLibs <84599917+GyverLibs@users.noreply.github.com>
2022-01-25 02:06:35 +0300

    Update ajaxPlot.ino

--- examples/ajaxPlot/ajaxPlot.ino
+++ examples/ajaxPlot/ajaxPlot.ino
@@ -1,23 +1,5 @@
 #include <GyverPortal.h>
-/*
-const char plt[] PROGMEM = R"rawliteral(
-<div id="chart-distance" class="container"></div>
-<script>
-var chartT = new Highcharts.Chart({chart:{renderTo:'chart-distance'},
-title:{text:'HC-SR04 Distance'},series:[{showInLegend:false,data:[]}],
-plotOptions: {line:{animation:false,dataLabels:{enabled:true}},series:{color: '#059e8a'}},
-xAxis:{type:'datetime',dateTimeLabelFormats:{second:'%H:%M:%S'}},
-yAxis:{title:{text:'Distance(CM)'}},credits:{enabled:false}});
 
-setInterval(function(){var xhttp=new XMLHttpRequest();
-xhttp.onreadystatechange=function(){if(this.readyState==4&&this.status==200){
-var x=(new Date()).getTime(),y=parseFloat(this.responseText);
-if(chartT.series[0].data.length > 40) chartT.series[0].addPoint([x, y],true,true,true);
-else chartT.series[0].addPoint([x, y],true,false,true);
-}};xhttp.open("GET","_GP_update?",true);xhttp.send();},1000);
-</script>
-)rawliteral";
-*/
 // билдер страницы
 void build() {
   String s;
